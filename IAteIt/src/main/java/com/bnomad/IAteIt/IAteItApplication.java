b6d463b91c42232d25661d62b8d0df4fbee27ce6
sunshiningsoo <yenkee1219@gmail.com>
2024-01-08 23:34:32 +0900

    init: project

---
+++ IAteIt/src/main/java/com/bnomad/IAteIt/IAteItApplication.java
@@ -0,0 +1,13 @@
+package com.bnomad.IAteIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAteItApplication.class, args);
+	}
+
+}
---
+++ IAteIt/src/test/java/com/bnomad/IAteIt/IAteItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnomad.IAteIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
