52b037d5106294ec02262101f80c2877b1a7b754
sunshiningsoo <yenkee1219@gmail.com>
2024-01-30 20:16:30 +0900

    refact: 임시 테스트 파일 삭제

--- IAteIt/src/test/java/com/bnomad/IAteIt/domain/block/BlockTest.java
+++ IAteIt/src/test/java/com/bnomad/IAteIt/domain/block/BlockTest.java
@@ -19,47 +19,5 @@ import java.util.List;
 @Transactional
 public class BlockTest {
 
-    @Autowired
-    private BlockService blockService;
-    @Autowired
-    private BlockRepository blockRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Test
-    @Rollback(value = false)
-    public void 멤버_block_test() {
-        Member member1 = MemberUtil.testMemberMemberRole();
-        Member member2 = MemberUtil.testMemberNoneRole();
-
-        memberRepository.save(member1);
-        memberRepository.save(member2);
-//        blockService.blockMember(member2.getId(), member1.getId());
-        member2.block_Member(member1);
-
-        List<Block> blocks = blockRepository.findAll();
-//        for (Block block : blocks) {
-//            System.out.println("blocked Member = " + block.getBlocked_member());
-//        }
-//        Assertions.assertThat(blocks.get(0).getBlocking_member().getId()).isEqualTo(member2.getId());
-//        Assertions.assertThat(blocks.get(0).getBlocked_member().getId()).isEqualTo(member1.getId());
-
-    }
-
-    @Test
-    public void Block객체로_멤버접근() {
-        Member member1 = MemberUtil.testMemberMemberRole();
-        Member member2 = MemberUtil.testMemberNoneRole();
-
-        memberRepository.save(member1);
-        memberRepository.save(member2);
-//        blockService.blockMember(member1.getId(), member2.getId());
-        member1.block_Member(member2);
-
-        for (Block block : member1.getBlockList()) {
-//            System.out.println("blocking, blocked id = " + block.getBlocking_member().getId() + " "+ block.getBlocked_member().getId());
-        }
-    }
 
 }
--- IAteIt/src/test/java/com/bnomad/IAteIt/domain/member/MemberCRUDTest.java
+++ IAteIt/src/test/java/com/bnomad/IAteIt/domain/member/MemberCRUDTest.java
@@ -17,33 +17,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 public class MemberCRUDTest {
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Test
-    @Rollback(value = false)
-    public void User_생성_테스트() {
-        Member member = Member.builder()
-                .nickname("user1")
-                .email("asdfasdas@google.com")
-                .profileImage("http://asdfasdfsdfsd")
-                .build();
-
-        memberRepository.save(member);
-        List<Member> all = memberRepository.findAll();
-        assertThat(all.size()).isEqualTo(1);
-    }
-
-    @Test
-    public void Member_modified_시간_테스트() {
-        Member member = memberRepository.findByNickname("user1");
-        String newName = "newName";
-//        member.editProfile(newName);
-        Member newMember = memberRepository.findByNickname(newName);
-//        assertThat(newMember.getModifiedDateTime().isAfter(newMember.getCreatedDateTime())).isTrue();
-        System.out.println("newMember = " + newMember.getCreatedDateTime());
-        System.out.println("newMember = " + newMember.getModifiedDateTime());
-    }
-
-
 }
